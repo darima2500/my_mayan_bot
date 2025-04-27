14b0af3288a822aa0ca3945869c25f4e99c241e9
darima2500 <darima2500@gmail.com>
2025-04-27 14:25:18 +0700

    Add files via upload

---
+++ tones/tones_data.py
@@ -0,0 +1,17 @@
+tones_data = {
+    1: {
+        "en": {
+            "name": "Magnetic Tone",
+            "keywords": "unity, purpose, initiation",
+            "description": "The Magnetic Tone marks the start of a new cycle..."
+        },
+        "ru": {
+            "name": "Магнитный тон",
+            "keywords": "единство, цель, инициирование",
+            "description": "Магнитный тон знаменует начало нового цикла..."
+        }
+    },
+    2: {
+        # И так далее до 13 тона
+    }
+}
