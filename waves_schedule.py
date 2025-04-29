1911341699827507ad5c75d2942cdf441a55c903
darima2500 <darima2500@gmail.com>
2025-04-29 11:56:06 +0700

    Update waves_schedule.py

--- waves_schedule.py
+++ waves_schedule.py
@@ -1,17 +1,27 @@
 from waves.yellow_star_wave import get_wave_message as get_yellow_star_wave_message
 from waves.red_dragon_wave import get_wave_message as get_red_dragon_wave_message
-# и так далее...
+from waves.white_jaguar_wave import get_wave_message as get_white_jaguar_wave_message
+# и так далее
+
 
 waves_schedule = [
     {
-        "start_date": "2025-04-25",
-        "end_date": "2025-05-07",
-        "get_message_func": get_yellow_star_wave_message
+        "name": "Yellow Star Wave",
+        "start_kin": 1,
+        "end_kin": 13,
+        "get_message_func": get_yellow_star_wave_message,
     },
     {
-        "start_date": "2025-05-08",
-        "end_date": "2025-05-20",
-        "get_message_func": get_red_dragon_wave_message
+        "name": "Red Dragon Wave",
+        "start_kin": 14,
+        "end_kin": 26,
+        "get_message_func": get_red_dragon_wave_message,
+    },
+    {
+        "name": "White Jaguar Wave",
+        "start_kin": 27,
+        "end_kin": 39,
+        "get_message_func": get_white_jaguar_wave_message,
     },
     # и так далее
 ]
